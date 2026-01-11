78246248c151edc8c42c1e21411608684e223de5
Yuganti-hash <yugantihatwar43@gmail.com>
2026-01-11 22:23:30 +0530

    Add PyTeal-based Algorand voting smart contract

--- contracts/voting.py
+++ contracts/voting.py
@@ -1,2 +1,31 @@
-# Placeholder for Algorand voting smart contract logic.
-# TODO: implement PyTeal contract.
+from pyteal import *
+
+def approval_program():
+    counter = Bytes("counter")
+
+    # On application creation
+    on_create = Seq(
+        App.globalPut(counter, Int(0)),
+        Return(Int(1))
+    )
+
+    # On NoOp call: increment counter
+    on_increment = Seq(
+        App.globalPut(counter, App.globalGet(counter) + Int(1)),
+        Return(Int(1))
+    )
+
+    program = Cond(
+        [Txn.application_id() == Int(0), on_create],
+        [Txn.on_completion() == OnComplete.NoOp, on_increment]
+    )
+
+    return program
+
+
+def clear_state_program():
+    return Return(Int(1))
+
+
+if __name__ == "__main__":
+    print(compileTeal(approval_program(), Mode.Application, version=6))
